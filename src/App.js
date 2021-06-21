103b3d47a40514119d9d0b8b78840d77946c925d
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-06-21 14:14:32 +0200

    style: :lipstick: React + MaterialUI Introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  testTitle: {
+    fontStyle: "oblique",
+    color: "red",
+  },
+  buttonStyles: {
+    color: "green",
+    border: 0,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography className={classes.testTitle} variant="h3" color="secondary">
+        Título de prueba
+      </Typography>
+      <Button
+        className={classes.buttonStyles}
+        variant="outlined"
+        color="secondary"
+      >
+        Este es mi primer botón con MaterialUI
+      </Button>
     </div>
   );
 }

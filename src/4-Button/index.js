0fdbedd9a1737b8384dd3ec8a4266c78bf287a88
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-06-22 13:09:11 +0200

    hotfix

---
+++ src/4-Button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Part4Button = () => {
+  return (
+    <div>
+      <h2>Funciona</h2>
+    </div>
+  );
+};
+
+export default Part4Button;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import Introduction from "./1-Introduction";
 // import CodingUXDesign from "./2-CodingAUXDesign";
-import MakeStyles from "./3-makeStyles";
+// import MakeStyles from "./3-makeStyles";
+import CustomButton from "./4-Button";
 
 function App() {
   return (
     <>
       {/* <Introduction /> */}
       {/* <CodingUXDesign /> */}
-      <MakeStyles />
+      {/* <MakeStyles /> */}
+      <CustomButton />
     </>
   );
 }
